b025b63705e06e0a99dd3868ddbee46936d5514d
JayeshKumar77 <7review777@gmail.com>
2024-04-25 09:29:39 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+const totalSlides = slides.length;
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    if (i === index) {
+      slide.style.display = 'block';
+    } else {
+      slide.style.display = 'none';
+    }
+  });
+}
+
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % totalSlides;
+  showSlide(currentSlide);
+}
+
+function prevSlide() {
+  currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
+  showSlide(currentSlide);
+}
+
+// Auto slide change
+setInterval(nextSlide, 3000); // Change slide every 3 seconds
