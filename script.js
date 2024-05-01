352245b1c0a614bb71265aa3aa183be0fd510525
JayeshKumar77 <7review7777@gmail.com>
2024-05-01 11:27:56 +0530

    adding images and javascript:adding fetch for images from file  and some some css

--- script.js
+++ script.js
@@ -1,26 +1,53 @@
-let currentSlide = 0;
-const slides = document.querySelectorAll('.slide');
-const totalSlides = slides.length;
+async function getImages() {
+  let a = await fetch("img/slider/");
+  let responce = await a.text();
+  let div = document.createElement("div");
+  div.innerHTML = responce;
+  let as = div.getElementsByTagName("a");
+  images = [];
+  for (let index = 0; index < as.length; index++) {
+      const element = as[index];
+      if ((element.href.endsWith(".webp")) || (element.href.endsWith(".png")) || (element.href.endsWith(".jpg")) || (element.href.endsWith(".jpeg"))) {
+          images.push(element.href.split(`/slider/`)[1]);
+      }
+  }
+  console.log(images)
 
-function showSlide(index) {
-  slides.forEach((slide, i) => {
-    if (i === index) {
-      slide.style.display = 'block';
-    } else {
-      slide.style.display = 'none';
-    }
-  });
+  let imgSlider = document.querySelector(".slider");
+  for (const image of images) {
+      imgSlider.innerHTML += `<div class="slide"><img src="img/slider/${image}" alt="Image 1"></div>`;
+  }
+
+  let currentSlide = 0;
+  const slides = document.querySelectorAll('.slide');
+  const totalSlides = slides.length;
+
+  function showSlide(index) {
+      slides.forEach((slide, i) => {
+          if (i === index) {
+              slide.style.display = 'block';
+          } else {
+              slide.style.display = 'none';
+          }
+      });
+  }
+
+  function nextSlide() {
+      currentSlide = (currentSlide + 1) % totalSlides;
+      showSlide(currentSlide);
+  }
+
+  function prevSlide() {
+      currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
+      showSlide(currentSlide);
+  }
+
+  // Setup event listeners after images are fetched and slider is created
+  document.querySelector('.prev').addEventListener('click', prevSlide);
+  document.querySelector('.next').addEventListener('click', nextSlide);
+
+  // Auto slide change
+  setInterval(nextSlide, 3000); // Change slide every 3 seconds
 }
 
-function nextSlide() {
-  currentSlide = (currentSlide + 1) % totalSlides;
-  showSlide(currentSlide);
-}
-
-function prevSlide() {
-  currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
-  showSlide(currentSlide);
-}
-
-// Auto slide change
-setInterval(nextSlide, 3000); // Change slide every 3 seconds
+getImages();
